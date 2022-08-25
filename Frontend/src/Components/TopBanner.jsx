3e54fd28f5e97a42f6e8ac45b567b2fc15ede3a6
sudipta bhattacharjee <sudiptabhattacharjee000@gmail.com>
2022-08-25 21:59:28 +0530

    day 3

--- Frontend/src/Components/TopBanner.jsx
+++ Frontend/src/Components/TopBanner.jsx
@@ -1,72 +1,119 @@
-import { Box, Button, Flex, Heading, Image, Input, Select, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
-import appBanner from '../Assets/HomePage_Images/Top-app-banner.webp'
-import QR from '../Assets/HomePage_Images/QR.webp'
-import SummerBanner from '../Assets/HomePage_Images/Summer-banner.jpg'
-
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  Select,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import appBanner from "../Assets/HomePage_Images/Top-app-banner.webp";
+import QR from "../Assets/HomePage_Images/QR.webp";
+import SummerBanner from "../Assets/HomePage_Images/Summer-banner.jpg";
 
 const TopBanner = () => {
+  return (
+    <Stack w={{ sm: "95%", md: "80%", lg: "80%" }} m="50px auto" gap="2.5rem">
+      {/* Download app banner */}
+      <Flex w="100%" borderRadius="8px" border="0.2px solid gray">
+        <Box w="35%">
+          <Image
+            h="100%"
+            w="100%"
+            borderTopLeftRadius="8px"
+            borderBottomLeftRadius="8px"
+            src={appBanner}
+          />
+        </Box>
 
-    return (
-        <Stack w={{sm: '95%', md: '80%', lg: '80%'}} m='50px auto' gap='2.5rem'>
+        <Flex w="70%" p="24px" justify="space-between" gap="1rem">
+          <Box w="75%">
+            <Heading>Our app takes you further</Heading>
+            <Text fontSize="sm" mt="0.5rem">
+              When you book on the app you can save up to 25% on select hotels
+              while earning double the points with every booking. With these app
+              deals you'll save even more on trips, and that means you can take
+              more trips, and manage it all on the go.
+            </Text>
 
-            {/* Download app banner */}
-            <Flex w='100%' borderRadius='8px' border='0.2px solid gray'>
-                <Box w='35%'>
-                    <Image h='100%' w='100%' borderTopLeftRadius='8px' borderBottomLeftRadius='8px' src={appBanner} />
-                </Box>
+            <Heading fontSize="lg" mt="1.5rem">
+              Text yourself a download link for easy access
+            </Heading>
+            <Flex align="center" gap="0.8rem">
+              <Select
+                borderColor="gray"
+                mt="1rem"
+                w="150px"
+                placeholder="Country code"
+              >
+                <option value="india +91"> India </option>
+                <option value="usa +1"> USA +1 </option>
+                <option value="germany +49"> Germany +49 </option>
+                <option value="ireland +353"> Ireland +353 </option>
+                <option value="hong kong SAR +852"> Hong Kong SAR +852 </option>
+                <option value="italy +39"> Italy +39 </option>
+                <option value="japan +81"> Japan +81 </option>
+                <option value="mexico +52"> Mexico +52 </option>
+                <option value="netherland +31"> Netherland +31 </option>
+                <option value="newzealand +64"> Newzealand +64 </option>
+              </Select>
 
-                <Flex w='70%' p='24px' justify='space-between' gap='1rem' >
-                    <Box w='75%'>
-                        <Heading>Our app takes you further</Heading>
-                        <Text fontSize='sm' mt='0.5rem'>
-                            When you book on the app you can save up to 25% on select hotels
-                            while earning double the points with every booking. With these app
-                            deals you'll save even more on trips,
-                            and that means you can take more trips, and manage it all on the go.
-                        </Text>
-
-                        <Heading fontSize='lg' mt='1.5rem'>Text yourself a download link for easy access</Heading>
-                        <Flex align='center' gap='0.8rem'>
-                            <Select borderColor='gray' mt='1rem' w='150px' placeholder='Country code'>
-                                <option value='india +91'> India </option>
-                                <option value='usa +1'> USA +1 </option>
-                                <option value='germany +49'> Germany +49 </option>
-                                <option value='ireland +353'> Ireland +353 </option>
-                                <option value='hong kong SAR +852'> Hong Kong SAR +852 </option>
-                                <option value='italy +39'> Italy +39 </option>
-                                <option value='japan +81'> Japan +81 </option>
-                                <option value='mexico +52'> Mexico +52 </option>
-                                <option value='netherland +31'> Netherland +31 </option>
-                                <option value='newzealand +64'> Newzealand +64 </option>
-                            </Select>
-
-                            <Input borderColor='gray' mt='1rem' w='150px' type='text' placeholder='Phone number' />
-                            <Button borderColor='gray' _hover='none' mt='1rem' w='150px' bg='#3662d8' color='white'> Get the app</Button>
-                        </Flex>
-
-                        <Text fontSize='xs' mt='0.5rem'>By providing your number, you agree to receive a one-time automated text message with a link to get the app. Standard text message rates may apply.</Text>
-                    </Box>
-
-                    {/* QR code box */}
-                    <Box w='29%'>
-                        <Image w='100%' h='80%' src={QR} />
-                        <Text textAlign='center'> Scan the QR code</Text>
-                    </Box>
-                </Flex>
+              <Input
+                borderColor="gray"
+                mt="1rem"
+                w="150px"
+                type="text"
+                placeholder="Phone number"
+              />
+              <Button
+                borderColor="gray"
+                _hover="none"
+                mt="1rem"
+                w="150px"
+                bg="#3662d8"
+                color="white"
+              >
+                {" "}
+                Get the app
+              </Button>
             </Flex>
 
-            {/* Summer offer banner */}
-            <Box w='100%' position='relative'>
-                <Image borderRadius='10px' w='100%' src={SummerBanner} />
-                <Box w='400px' position='absolute' top='50px' left='50px'>
-                    <Heading color='white'>Members get last minute summer savings</Heading>
-                    <Text m='1rem 0px' color='white'>Plan a last-minute summer getaway with Member Prices of 10% off or more on select hotels.</Text>
-                    <Button bg='#3662d8' _hover='none' color='white'>See members details</Button>
-                </Box>
-            </Box>
-        </Stack>
-    )
-}
+            <Text fontSize="xs" mt="0.5rem">
+              By providing your number, you agree to receive a one-time
+              automated text message with a link to get the app. Standard text
+              message rates may apply.
+            </Text>
+          </Box>
 
-export default TopBanner
+          {/* QR code box */}
+          <Box w="29%">
+            <Image w="100%" h="80%" src={QR} />
+            <Text textAlign="center"> Scan the QR code</Text>
+          </Box>
+        </Flex>
+      </Flex>
+
+      {/* Summer offer banner */}
+      <Box w="100%" position="relative">
+        <Image borderRadius="10px" w="100%" src={SummerBanner} />
+        <Box w="400px" position="absolute" top="50px" left="50px">
+          <Heading color="white">
+            Members get last minute summer savings
+          </Heading>
+          <Text m="1rem 0px" color="white">
+            Plan a last-minute summer getaway with Member Prices of 10% off or
+            more on select hotels.
+          </Text>
+          <Button bg="#3662d8" _hover="none" color="white">
+            See members details
+          </Button>
+        </Box>
+      </Box>
+    </Stack>
+  );
+};
+
+export default TopBanner;
---
+++ Frontend/src/Pages/S_CreateAccount.jsx
@@ -0,0 +1,169 @@
+import {
+    Box,
+    Input,
+    Text,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    Checkbox,
+    Button,
+    Alert,
+  } from "@chakra-ui/react";
+  import React from "react";
+  import { AiFillApple } from "react-icons/ai"; // FcGoogle
+  import { FcGoogle } from "react-icons/fc";
+  import { AiFillFacebook } from "react-icons/ai";
+  import { useState } from "react";
+  import { useDispatch } from "react-redux";
+  import { Signigfun } from "../Redux/Auth-reducer/action";
+  import { Link } from "react-router-dom";
+  
+  export default function CreateAccount() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName,setFirstName] = useState('')
+    const [lastName,setLastName] = useState('')
+    const dispatch = useDispatch()
+  
+    function SendSignInRequest() {
+    //   if(email !== '' && password !== ''){
+    //     dispatch(Signigfun({email: email, password: password}))
+    //   }
+    if(email === '' || password === '' || firstName === '' || lastName === ''){
+        alert('Fill Form')
+    }
+    }
+    return (
+      <Box>
+        <Box h={"60px"} border={"1px solid black"}></Box>
+  
+        <Box
+          margin={"auto"}
+          w={"30%"}
+          h={"600px"}
+          // border={"1px solid black"}
+          marginTop={"40px"}
+        >
+          <Box mb={"20px"}>
+            <Text fontSize={"4xl"} fontWeight={"600"}>
+            Create an account
+            </Text>
+          </Box>
+          <Box mb={"20px"}>
+          <FormLabel>Enter Email</FormLabel>
+            <Input
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email address"
+              w={"100%"}
+              h={"55px"}
+              border={`2px solid`}
+              type={"email"}
+            />
+          {/* {email === '' ? <FormLabel color={'red'}>Email is required.</FormLabel>:<FormLabel></FormLabel>} */}
+          </Box>
+          <Box mb={"20px"}>
+          <FormLabel>Enter First name</FormLabel>
+            <Input
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="First name"
+              w={"100%"}
+              h={"55px"}
+              border={`2px solid`}
+              type={"text"}
+            />
+          {/* {firstName === '' ? <FormLabel color={'red'}>First name is required.</FormLabel>:<FormLabel></FormLabel>} */}
+          </Box>
+          {/* last name */}
+          <Box mb={"20px"}>
+          <FormLabel>Enter Last name</FormLabel>
+            <Input
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Last name"
+              w={"100%"}
+              h={"55px"}
+              border={`2px solid`}
+              type={"text"}
+            />
+          {/* {lastName === '' ? <FormLabel color={'red'}>Last name is required.</FormLabel>:<FormLabel></FormLabel>} */}
+          </Box>
+          {/* password */}
+          <Box mb={"20px"}>
+            <FormLabel>Enter Password</FormLabel>
+            <Input
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              w={"100%"}
+              h={"55px"}
+              border={`2px solid`}
+              type={"password"}
+            />
+          {/* {password === '' ? <FormLabel color={'red'}>Password is required.</FormLabel>:<FormLabel></FormLabel>} */}
+          </Box>
+          <Box mb={"20px"}>
+            <Checkbox size={"lg"} defaultChecked>
+              Keep me signed in
+            </Checkbox>
+          </Box>
+          <Box>
+            <Text></Text>
+          </Box>
+          <Box mb={"20px"}>
+            <Box>
+              <label>By signing in, I agree to the Expedia</label>{" "}
+              <label style={{ color: "blue", cursor: "pointer" }}>
+                Terms and Conditions,
+              </label>
+            </Box>
+            <Box>
+              <label style={{ color: "blue", cursor: "pointer" }}>
+                Privacy Statement{" "}
+              </label>
+              <label>and </label>
+              <label style={{ color: "blue", cursor: "pointer" }}>
+                Expedia Rewards Terms and Conditions.
+              </label>
+            </Box>
+          </Box>
+          <Box>
+            <Button
+              onClick={SendSignInRequest}
+              w={"100%"}
+              bg={"#0693e3"}
+              h={"55px"}
+            >
+              Sign in
+            </Button>
+          </Box>
+          <Box mt={"15px"} display="flex" justifyContent={"center"}>
+            {/* < style={{}}>Forgot password?</> */}
+            <Link to={''} style={{color: "blue"}}>Forgot password?</Link>
+          </Box>
+          <Box mt={"15px"} display="flex" justifyContent={"center"}>
+            <label htmlFor="">Already have an account? </label>
+            <label style={{ color: "white" }} htmlFor="">
+              o
+            </label>
+            <Link to={'/signin'} style={{color: "blue"}}> Sign In</Link>
+          </Box>
+          <Box mt={"25px"} display="flex" justifyContent={"center"}>
+            <label htmlFor="">or continue with</label>
+          </Box>
+          <Box mt={"10px"} display="flex" justifyContent={"center"}>
+            <AiFillApple
+              size={"25px"}
+              style={{ marginRight: "10px", cursor: "pointer" }}
+            />
+            <FcGoogle
+              size={"25px"}
+              style={{ marginRight: "10px", cursor: "pointer" }}
+            />
+            <AiFillFacebook
+              size={"25px"}
+              style={{ marginRight: "10px", cursor: "pointer" }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    );
+  }
+  
