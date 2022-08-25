4ba4528a2acb4e037f2955c6db4973032a5f0a31
Bhrugu <bhrugupadhi@gmail.com>
2022-08-25 23:25:21 +0530

    day-3 file added

--- Frontend/src/Components/MainRoutes.jsx
+++ Frontend/src/Components/MainRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import HomePage from '../Pages/HomePage'
+import { Totalpage } from './Totalpage'
 
 const MainRoutes = () => {
     return (
-
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/login'  />
+            <Route path='/payment' element={<Totalpage />}/>
         </Routes>
     )
 }
--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Box, Container } from '@chakra-ui/react'
 import React from 'react'
 import MiddleBanner from '../Components/MiddleBanner'
 import MiddleCards from '../Components/MiddleCards'
-import Navbar from '../Components/Navbar'
+import { Navbar } from '../Components/Navbar'
 import TopBanner from '../Components/TopBanner'
 
 const HomePage = () => {
