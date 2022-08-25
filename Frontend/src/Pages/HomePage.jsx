6a181305fdb61504a7f90d16ecfb9b660b4b349e
Md Irfan <irfanjunaid78081@gmail.com>
2022-08-25 21:10:52 +0530

    day-3 issue completed

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import MiddleBanner from '../Components/MiddleBanner'
 import MiddleCards from '../Components/MiddleCards'
 import Navbar from '../Components/Navbar'
+import ScrollBarBox from '../Components/ScrollBarBox'
 import TopBanner from '../Components/TopBanner'
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
             <TopBanner />
             <MiddleCards/>
             <MiddleBanner/>
+            <ScrollBarBox/>
         </Box>
     )
 }
