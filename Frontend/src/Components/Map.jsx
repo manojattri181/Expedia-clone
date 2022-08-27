c1679aa46dd8e2506761cb982de3c14bfc8049d4
mohammad-atique <92262714+mohammad-atique@users.noreply.github.com>
2022-08-27 20:51:02 +0530

    completed Today's Task

--- Frontend/src/Components/Map.jsx
+++ Frontend/src/Components/Map.jsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react'
 import { Link, useSearchParams } from 'react-router-dom';
 
 
-const Map = () => {
+const Map = ({city="delhi",width="240px",height="220px"}) => {
+  console.log(width,height)
 let [searchparam] =useSearchParams();
-let city = searchparam.get("_city");
+let _city = searchparam.get("_city");
   useEffect(()=>{
-  },[searchparam,city])
+  },[searchparam,_city])
   return (
     <div className="w-fit pb-4 m-auto  text-center bg-white border-1 border-solid border-black  rounded-2xl" >
-        <iframe title={city} width="240px" height="120" src={`https://maps.google.com/maps?q=${city || "delhi"}&t=&z=11&ie=UTF8&iwloc=&output=embed`} scrolling="no"></iframe>
+        <iframe title={city} width={width} height={height} src={`https://maps.google.com/maps?q=${city || "delhi"}&t=&z=11&ie=UTF8&iwloc=&output=embed`} scrolling="no"></iframe>
         <Link to="">
             <h3 className='text-sm font-semibold text-blue mt-4 hover:underline' >View in map</h3>
         </Link>
